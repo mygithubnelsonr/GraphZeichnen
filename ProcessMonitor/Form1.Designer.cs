41459fda186d99b38d2a345e07812fe8d0338836
Robert Nelson <reg0204@sys-service.de>
2019-06-08 14:42:25 +0200

    first commit

---
+++ ProcessMonitor/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace ProcessMonitor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panelGraph = new System.Windows.Forms.Panel();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.labelCPU = new System.Windows.Forms.Label();
+            this.labelProcesse = new System.Windows.Forms.Label();
+            this.labelSpeicher = new System.Windows.Forms.Label();
+            this.toolTipTrackbar = new System.Windows.Forms.ToolTip(this.components);
+            this.panelControls = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.panelControls.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelGraph
+            // 
+            this.panelGraph.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panelGraph.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.panelGraph.Location = new System.Drawing.Point(0, 0);
+            this.panelGraph.Name = "panelGraph";
+            this.panelGraph.Size = new System.Drawing.Size(434, 164);
+            this.panelGraph.TabIndex = 0;
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(25, 8);
+            this.buttonStart.Margin = new System.Windows.Forms.Padding(3, 8, 3, 8);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(75, 23);
+            this.buttonStart.TabIndex = 1;
+            this.buttonStart.Text = "&Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(25, 47);
+            this.trackBar1.Margin = new System.Windows.Forms.Padding(3, 8, 3, 3);
+            this.trackBar1.Maximum = 20;
+            this.trackBar1.Minimum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(104, 45);
+            this.trackBar1.TabIndex = 2;
+            this.trackBar1.Value = 10;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(289, 6);
+            this.label1.Margin = new System.Windows.Forms.Padding(8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "CPU";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Green;
+            this.label2.Location = new System.Drawing.Point(262, 35);
+            this.label2.Margin = new System.Windows.Forms.Padding(8);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Processe";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Blue;
+            this.label3.Location = new System.Drawing.Point(219, 64);
+            this.label3.Margin = new System.Windows.Forms.Padding(8);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(102, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Speichernutzung";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // labelCPU
+            // 
+            this.labelCPU.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelCPU.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.labelCPU.Location = new System.Drawing.Point(337, 6);
+            this.labelCPU.Margin = new System.Windows.Forms.Padding(8);
+            this.labelCPU.Name = "labelCPU";
+            this.labelCPU.Size = new System.Drawing.Size(80, 13);
+            this.labelCPU.TabIndex = 6;
+            this.labelCPU.Text = " ";
+            // 
+            // labelProcesse
+            // 
+            this.labelProcesse.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelProcesse.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.labelProcesse.Location = new System.Drawing.Point(337, 35);
+            this.labelProcesse.Margin = new System.Windows.Forms.Padding(8);
+            this.labelProcesse.Name = "labelProcesse";
+            this.labelProcesse.Size = new System.Drawing.Size(80, 13);
+            this.labelProcesse.TabIndex = 7;
+            this.labelProcesse.Text = " ";
+            // 
+            // labelSpeicher
+            // 
+            this.labelSpeicher.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelSpeicher.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.labelSpeicher.Location = new System.Drawing.Point(337, 64);
+            this.labelSpeicher.Margin = new System.Windows.Forms.Padding(8);
+            this.labelSpeicher.Name = "labelSpeicher";
+            this.labelSpeicher.Size = new System.Drawing.Size(80, 13);
+            this.labelSpeicher.TabIndex = 8;
+            this.labelSpeicher.Text = " ";
+            // 
+            // panelControls
+            // 
+            this.panelControls.Controls.Add(this.buttonStart);
+            this.panelControls.Controls.Add(this.labelSpeicher);
+            this.panelControls.Controls.Add(this.trackBar1);
+            this.panelControls.Controls.Add(this.labelProcesse);
+            this.panelControls.Controls.Add(this.labelCPU);
+            this.panelControls.Controls.Add(this.label1);
+            this.panelControls.Controls.Add(this.label3);
+            this.panelControls.Controls.Add(this.label2);
+            this.panelControls.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panelControls.Location = new System.Drawing.Point(0, 167);
+            this.panelControls.Name = "panelControls";
+            this.panelControls.Size = new System.Drawing.Size(434, 94);
+            this.panelControls.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(434, 261);
+            this.Controls.Add(this.panelControls);
+            this.Controls.Add(this.panelGraph);
+            this.MinimumSize = new System.Drawing.Size(450, 300);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.ResizeBegin += new System.EventHandler(this.Form1_ResizeBegin);
+            this.ResizeEnd += new System.EventHandler(this.Form1_ResizeEnd);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.panelControls.ResumeLayout(false);
+            this.panelControls.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelGraph;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label labelCPU;
+        private System.Windows.Forms.Label labelProcesse;
+        private System.Windows.Forms.Label labelSpeicher;
+        private System.Windows.Forms.ToolTip toolTipTrackbar;
+        private System.Windows.Forms.Panel panelControls;
+    }
+}
+
---
+++ ProcessMonitor/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ProcessMonitor
+{
+    public partial class Form1 : Form
+    {
+        private Single _X;
+        private Single _sngY;
+        private Single _cpuY;
+        private Single _proY;
+        private Single _memY;
+        private int _graphHeight;
+        private int _graphWidth;
+        private int _line1Y;
+        private int _line2Y;
+        private int _line3Y;
+        private bool remove = true;
+        private bool draw = false;
+
+        Random rndcpu = new Random(123);
+        Random rndpro = new Random(456);
+        Random rndmem = new Random(789);
+
+        Graphics _graphics;
+
+        Pen penCPU = new Pen(Color.Red);
+        Pen penProc = new Pen(Color.Green);
+        Pen penMem = new Pen(Color.Blue);
+        Pen penFore = new Pen(SystemColors.ActiveCaption);
+        Pen penBack = new Pen(SystemColors.ControlLight);
+        Pen penLine = new Pen(Color.Black);
+
+        const Single INCREMENT = 5;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            toolTipTrackbar.SetToolTip(trackBar1, timer1.Interval.ToString() + "ms");
+
+            _graphHeight = panelGraph.Height;
+            _graphWidth = panelGraph.Width;
+            _line1Y = _graphHeight / 4 * 1;
+            _line2Y = _graphHeight / 4 * 2;
+            _line3Y = _graphHeight / 4 * 3;
+
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            if (buttonStart.Text == "&Start")
+            {
+                buttonStart.Text = "&Stop";
+                timer1.Enabled = true;
+            }
+            else
+            {
+                buttonStart.Text = "&Start";
+                timer1.Enabled = false;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Single cpu = rndcpu.Next(_graphHeight);
+            Single pro = rndpro.Next(_graphHeight);
+            Single mem = rndmem.Next(_graphHeight);
+
+            Zeichnen(cpu, pro, mem);
+
+            labelCPU.Text = cpu.ToString();
+            labelProcesse.Text = pro.ToString();
+            labelSpeicher.Text = mem.ToString();
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            int value = (int)trackBar1.Value;
+            timer1.Interval = value * 100;
+            toolTipTrackbar.SetToolTip(trackBar1, timer1.Interval.ToString() + "ms");
+        }
+
+        private void Zeichnen(Single cpuWert, Single proWert, Single memWert)
+        {
+            //Graphics graphics = panel1.CreateGraphics();
+            Single height = _graphHeight;
+            Single cpuY;
+            Single proY;
+            Single memY;
+
+            _graphics.DrawLine(penBack, _X, 0, _X, height);
+
+            cpuY = height - (cpuWert) - 1;
+            _graphics.DrawLine(penCPU, _X - INCREMENT, _cpuY, _X, cpuY);
+            _cpuY = cpuY;
+
+            proY = height - (proWert) - 1;
+            _graphics.DrawLine(penProc, _X - INCREMENT, _proY, _X, proY);
+            _proY = proY;
+
+            memY = height - (memWert) - 1;
+            _graphics.DrawLine(penMem, _X - INCREMENT, _memY, _X, memY);
+            _memY = memY;
+
+            _X += INCREMENT;
+
+            if (_X > panelGraph.Width)
+            {
+                _X = 0;
+                _graphics.Clear(SystemColors.ControlLight);
+            }
+
+            _graphics.DrawLine(penFore, _X, 0, _X, height);
+
+        }
+
+        private void Form1_ResizeEnd(object sender, EventArgs e)
+        {
+            _graphHeight = panelGraph.Height;
+            _graphWidth = panelGraph.Width;
+
+            _line1Y = _graphHeight / 4 * 1;
+            _line2Y = _graphHeight / 4 * 2;
+            _line3Y = _graphHeight / 4 * 3;
+
+            DrawLines(remove);
+            DrawLines(draw);
+
+        }
+
+        private void Form1_ResizeBegin(object sender, EventArgs e)
+        {
+            DrawLines(remove);
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            DrawLines(draw);
+        }
+
+        private void DrawLines(bool act)
+        {
+            if (act == remove)
+            {
+                _graphics.DrawLine(penBack, 0, _line1Y, _graphWidth, _line1Y);
+                _graphics.DrawLine(penBack, 0, _line2Y, _graphWidth, _line2Y);
+                _graphics.DrawLine(penBack, 0, _line3Y, _graphWidth, _line3Y);
+            }
+            else
+            {
+                _graphics = panelGraph.CreateGraphics();
+                _graphics.DrawLine(penLine, 0, _line1Y, panelGraph.Width, _line1Y);
+                _graphics.DrawLine(penLine, 0, _line2Y, panelGraph.Width, _line2Y);
+                _graphics.DrawLine(penLine, 0, _line3Y, panelGraph.Width, _line3Y);
+            }
+        }
+    }
+}
